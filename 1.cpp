fe33949bc4897ce623693ad32c83a891f3e904ae
SonaSudebta <sudebtam@gmail.com>
2024-10-10 15:02:28 +0700

    this is amazing

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>;
+#include <string>;
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!";
+	string name;
+
+	cout << "Please input your name:";
+	cin >> name;
+	cout << endl;
+
+	cout << "Your name is: " << name;
 
 	return 0;
 }
